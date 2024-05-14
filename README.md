b6f4b97f48ac104fec0c6732b849ac77ec4b6a95
kidclone3 <kid.clone.3@gmail.com>
2024-05-14 08:03:52 +0700

    add computational results

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DDD-thesis
+
+## Note some tricks
+
+1. If `biber` failed to run (check the `$? != 0`), clean the cache by 
+    ```
+    rm -rf `biber --cache`
+    ``` 
+    and re-run the `biber` command.
---
+++ sections/edited-images/crop_images.py
@@ -0,0 +1,27 @@
+# Python script to crop images using OpenCV 
+
+import cv2
+import os
+
+# Function to crop images
+
+def crop_images(input_folder, image_name, x, y, w, h):
+    img = cv2.imread(os.path.join(input_folder, image_name))
+    crop_img = img[y:y+h, x:x+w, :]
+    cv2.imwrite(os.path.join(input_folder, f"{image_name}"), crop_img)
+
+if __name__ == '__main__':
+
+    # Input folder path
+    input_folder = './'
+
+    # Crop coordinates
+    x = 0
+    y = 0
+
+    # Width and height of the cropped image
+    w = 347
+    h = 294
+
+    # Call the function
+    crop_images(input_folder, "Figure4e.jpg", x, y, w, h)
